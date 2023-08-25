4fb167489f651dae7ab08e8730b47a85b1e26ae2
Doa'a Al Shawwa <124186473+doaaa04@users.noreply.github.com>
2023-08-25 21:29:13 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main{
+	public static void main(String[] args) {
+		Scanner scan =new Scanner (System.in);
+		String one =scan.next().toLowerCase();
+		String two =scan.next().toLowerCase();
+		int num =0;
+		if (one.equals(two)){num=0;}
+		   else {
+		       	for (int i =0;i<one.length();i++){
+		   if (one.charAt(i)>two.charAt(i)){num =1; break;}
+		   else if (one.charAt(i)<two.charAt(i)){
+		       num =-1; 
+		   break;
+		       
+		   }
+		}
+		   }
+		
+	
+	
+		
+		System.out.println(num);
+		
+	}
